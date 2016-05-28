7dd12a7336575c465aefe2553cf0aacea2df3d61
vijaysraghavan <vijay@vebinary.com>
2016-05-28 03:41:57 -0400

    Update events.php

--- events.php
+++ events.php
@@ -134,6 +134,23 @@
                                         impact at the bedside.
                                     </div>
                                     
+                                    <?php
+                                        $contact_sfid = $_SESSION['contact_sfid'];
+                                        $status = registeredStatus('AACN National Teaching Institute & Critical Care Exposition', $contact_sfid);
+                                        if ($status) {
+                                    ?>
+                                    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                                        <div class="submit-wraper">
+                                            <input type="hidden" value="<?php echo $_SESSION['contact_sfid']; ?>" name="contact_sfid">
+                                            <input type="hidden" value="AACN National Teaching Institute & Critical Care Exposition" name="title">
+                                            <div class="button">Cancel Event
+                                                <input type="submit" name="CancelEvent"/>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <?php } else {
+                                        $contact_sfid = $_SESSION['contact_sfid'];
+                                    ?>
                                     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" name="registerform">
                                         <div class="submit-wraper">
                                             <input type="hidden" value="<?php echo $_SESSION['contact_sfid']; ?>" name="contact_sfid">
@@ -143,6 +160,7 @@
                                             </div>
                                         </div>
                                     </form>
+                                    <?php } ?>
                                     
                                 </div>
 
