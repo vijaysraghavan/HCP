70b67ae8e3b1ccec1fc7baa961d177a1212aa2ba
vijaysraghavan <vijay@vebinary.com>
2016-05-28 00:26:50 -0400

    Update events.php

--- events.php
+++ events.php
@@ -136,7 +136,7 @@
                                     
                                     <?php
                                         $contact_sfid = $_SESSION['contact_sfid'];
-                                        $status = registeredStatus('AACN National Teaching Institute & Critical Care Exposition', '$contact_sfid');
+                                        $status = registeredStatus('AACN National Teaching Institute & Critical Care Exposition', $contact_sfid);
                                         if ($status) {
                                     ?>
                                     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
