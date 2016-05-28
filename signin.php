f1e4be7ed9c2b34d0d9d0f808490aed4650ea688
vijaysraghavan <vijay@vebinary.com>
2016-05-28 05:50:54 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -115,10 +115,10 @@
                             <input class="required" type="text" placeholder="Your email" value="" name="email"/>
                             <input type="password" placeholder="Your password" value="" name="password"/>
                             <input type="hidden" name="mailto" value="info@vebinary.com" style="display: none;" />
-                            
+                            <a href="#">Forgot password</a>
+                            <div class="description">One account is all you need</div>
                             <div class="submit-wraper">
                                 <div class="button">Sign In
-                                    <a href="#">Forgot password</a>
                                     <input type="submit" value="" name="login"/>
                                 </div>
 
