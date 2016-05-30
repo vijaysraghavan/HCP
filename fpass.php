5027c3a75a6b48d2fcefb0bb977c7cb0c607f088
vijaysraghavan <vijay@vebinary.com>
2016-05-30 10:42:09 -0400

    Update fpass.php

--- fpass.php
+++ fpass.php
@@ -1,3 +1,40 @@
 <?php
-	echo "Hey, this is some text!";
+	<form class="login-form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+	
+	    <input class="required" type="text" placeholder="Your email" value="" name="email"/>
+	    
+	    <div class="submit-wraper">
+	        <div class="button">Sign In
+	            <input type="submit" value="" name="forgotpassword"/>
+	        </div>
+	
+	    </div>
+	
+	</form>
+        
+        if (isset($_POST['forgotpassword'])) {
+	    //Post Values
+	    $mailto = pg_escape_string($_POST['email']);
+	    $password = getPassword($mailto);
+	    
+	    $subject = "Your Cintria password";
+	    $message = "Your password is " + $password;
+	    $mailstatus = mail($mailto, $subject, $message);
+	    if ($mailstatus) {
+	        ?>
+	        <script type="text/javascript">
+	            $.notify("Email sent successfully.", 'success')
+	        </script>
+	    <?php
+	    }
+	    else{
+	    ?>
+	        <script type="text/javascript">
+	            $.notify("Sending email failed.", {
+	                style: 'bootstrap'
+	            });
+	        </script>
+	        <?php
+	    }
+	}
 ?>
