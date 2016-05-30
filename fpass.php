5bc5d1db41ffe78906f988190a558647b8c92563
vijaysraghavan <vijay@vebinary.com>
2016-05-30 11:09:10 -0400

    Update fpass.php

--- fpass.php
+++ fpass.php
@@ -1,18 +1,5 @@
 <?php
-	<form method='post' action='sendemail.php' >
-	    <label>Name</label>
-	    <input name="name" placeholder="Type Here">
-	    <label>Email</label>
-	    <input name="email" placeholder="Type Here" id="email">
-	    <label>Message</label>
-	    <textarea name="message" placeholder="Type Here"></textarea>
-	
-	
-	    <label>Human Verification</label>
-	      <input name="human" placeholder="2 + 2 = ? " id="human">
-	      <input id="submit" name="submit" type="submit" value="Submit">
-	    </label>     
-	</form>
+	Yasaswi
         
         
 ?>
