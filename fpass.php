78286d221998f6f602bb869dbd6d3063f973386d
vijaysraghavan <vijay@vebinary.com>
2016-05-31 10:02:19 -0400

    Update fpass.php

--- fpass.php
+++ fpass.php
@@ -109,7 +109,8 @@ include_once "functions/functions.php";
 
 if (isset($_POST['emailpassword'])) {
     $name = 'Cintria Admin';
-    $email = $_POST['email'];
+    //$email = $_POST['email'];
+    $email = 'jennifer.vebinary@gmail.com';
     $password = getPassword($email);
     $message = 'Your password is ' + $password;
     $from = 'info@vebinary.com'; 
